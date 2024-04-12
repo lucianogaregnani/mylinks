e807ebc29c3dad398c2d325605c9904edf996c3d
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-04-12 00:21:10 -0300

    init

---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqjwAddct0AYW8EJVd5Mk0eWumDnbLfRM",
+  authDomain: "linktree-cce90.firebaseapp.com",
+  projectId: "linktree-cce90",
+  storageBucket: "linktree-cce90.appspot.com",
+  messagingSenderId: "930685222453",
+  appId: "1:930685222453:web:2345184bcf8988f1643b7c"
+};
+
+export const app = initializeApp(firebaseConfig);
