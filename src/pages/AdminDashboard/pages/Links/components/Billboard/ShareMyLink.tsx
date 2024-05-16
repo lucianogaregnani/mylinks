d86c67d53da9e8a0ea1f569b4af41fe173b5261c
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-05-16 17:36:55 -0300

    copy link

--- src/pages/AdminDashboard/pages/Links/components/Billboard/ShareMyLink.tsx
+++ src/pages/AdminDashboard/pages/Links/components/Billboard/ShareMyLink.tsx
@@ -1,8 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Link } from "react-router-dom";
 import AlertIcon from "../Icons/AlertIcon";
 import Button from "../../../../../../components/Button/Button";
+import useSettings from "../../../../../../hooks/useSettings";
+import useAuth from "../../../../../../hooks/useAuth";
+import { useState } from "react";
+import OkIcon from "../Icons/OkIcon";
 
 function ShareMyLink() {
+  const { currentUser } = useAuth();
+  const { username } = useSettings({ userId: currentUser?.uid });
+  const URL = `${import.meta.env.VITE_URL_VIEW}/${username}`;
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(URL);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
   return (
     <article className="shadow-sm md:rounded-3xl bg-[rgb(224,233,250,1)] w-full p-[0.6rem]">
       <div className="flex md:flex-row items-start gap-3 w-full">
@@ -11,14 +29,14 @@ function ShareMyLink() {
           <div>
             <h3>
               <span className="font-semibold">Your MyLink is live:</span>
-              <Link className="underline" to="https://linktr.ee/whitertroll">
-                mylinks.com/garegnani
+              <Link className="underline ml-1" target="_blank" to={URL}>
+                {URL}
               </Link>
             </h3>
             <p>Share your MyLink to your socials</p>
           </div>
-          <Button color="white" size="small">
-            Copy URL
+          <Button onClick={handleCopy} color="white" size="small">
+            {copied ? <OkIcon /> : <p>Copy URL</p>}
           </Button>
         </div>
       </div>
