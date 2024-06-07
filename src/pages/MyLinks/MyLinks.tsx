2f37ba9061f9e8aa1101970afcc36e9eedba302c
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-06-07 00:58:47 -0300

    fix

--- src/pages/MyLinks/MyLinks.tsx
+++ src/pages/MyLinks/MyLinks.tsx
@@ -6,9 +6,19 @@ import useUserLinks from "./hooks/useUserLinks";
 
 function MyLinks() {
   const { username } = useParams();
-  const { setting, settingBillboardLoading } = useSettingBillboard(username);
+  const { setting, settingBillboardLoading, error } =
+    useSettingBillboard(username);
   const { photoURL, userPhotoLoading } = useUserPhoto(setting?.userId);
   const { links, userLinksLoading } = useUserLinks(setting?.userId);
+
+  if (error)
+    return (
+      <main className="text-slate-600 flex flex-col gap-2 items-center justify-center h-screen w-full">
+        <h1 className="text-6xl font-bold text-indigo-500">Ups</h1>
+        <p className="text-2xl font-semibold">There was an error!</p>
+        <p className="text-4xl">{error}</p>
+      </main>
+    );
 
   return (
     <main className="h-screen w-full">
--- src/pages/MyLinks/hooks/useSettingBillboard.ts
+++ src/pages/MyLinks/hooks/useSettingBillboard.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { Setting } from "../../../types/Settings.type";
@@ -7,38 +8,48 @@ import { db } from "../../../config/firebase";
 function useSettingBillboard(username: string | undefined) {
   const [setting, setSetting] = useState<Setting | undefined>();
   const [settingBillboardLoading, setSettingBillboardLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const getUserInfo = async () => {
-    let settingRes: Setting = {
-      id: "",
-      username: "",
-      title: "",
-      userId: "",
-      type: "elegant",
-    };
-    const refSettings = collection(db, "setting");
+    try {
+      const refSettings = collection(db, "setting");
 
-    const q = query(refSettings, where("username", "==", username));
+      const q = query(refSettings, where("username", "==", username));
 
-    setSettingBillboardLoading(true);
-    const docResponse = await getDocs(q);
+      setSettingBillboardLoading(true);
+      const docResponse = await getDocs(q);
 
-    docResponse.forEach((doc) => {
-      settingRes = {
-        id: doc.id,
-        ...doc.data(),
-      } as Setting;
-    });
+      let settingRes: Setting = {
+        id: "",
+        username: "",
+        title: "",
+        userId: "",
+        type: "elegant",
+      };
 
-    setSetting(settingRes);
-    setSettingBillboardLoading(false);
+      if (docResponse.empty) {
+        throw new Error("The user doesn't exists");
+      }
+
+      docResponse.forEach((doc) => {
+        settingRes = {
+          id: doc.id,
+          ...doc.data(),
+        } as Setting;
+      });
+
+      setSetting(settingRes);
+      setSettingBillboardLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+    }
   };
 
   useEffect(() => {
     getUserInfo();
   }, []);
 
-  return { setting, settingBillboardLoading };
+  return { setting, settingBillboardLoading, error };
 }
 
 export default useSettingBillboard;
