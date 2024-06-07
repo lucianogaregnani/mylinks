0b686525b75c6d97762ac7b94a88899b21d0df41
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-06-07 18:29:17 -0300

    home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,26 @@
+import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
+import image from "./assets/281shots_so.png";
+
+const LINK = import.meta.env.VITE_URL_VIEW;
 
 function Home() {
   return (
-    <main className="bg-[#254f1a] h-screen">
-      <header className="gap-2 flex justify-center w-[60%] bg-white rounded-b-full mx-auto p-2">
-        <Button size="large" color="white">
-          Log In
-        </Button>
-        <Button color="secondary" size="large">
-          Sign up free
-        </Button>
+    <main className="bg-[#254f1a] md:gap-0 gap-4 flex flex-col md:h-screen">
+      <header className="bg-green-600 gap-2 flex justify-center w-full md:w-[60%] rounded-b-full mx-auto p-2">
+        <Link to={`${LINK}/login`}>
+          <Button size="large" color="white">
+            Log In
+          </Button>
+        </Link>
+        <Link to={`${LINK}/register`}>
+          <Button color="secondary" size="large">
+            Sign up free
+          </Button>
+        </Link>
       </header>
-      <main className="flex items-center h-full text-[#d2e823] p-10">
-        <div className="w-2/4">
+      <main className="flex md:flex-row flex-col items-center justify-around h-full text-[#d2e823] px-10">
+        <div className="md:w-2/4">
           <h1 className="font-bold text-6xl">
             Everything you are. In one, simple link in bio.
           </h1>
@@ -22,8 +30,11 @@ function Home() {
             profiles.
           </p>
         </div>
+        <div className="md:w-[40%] w-[25rem]">
+          <img className="rounded-2xl border-2" src={image} alt="" />
+        </div>
       </main>
-      <footer></footer>
+      <footer className="flex items-center justify-center text-2xl font-bold bg-green-600 h-20 w-full rounded-t-2xl"></footer>
     </main>
   );
 }
